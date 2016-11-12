f12015ba8391d8409f82ddb89dafb531bf0d9848
manishsingh10895 <manishsingh10895@gmail.com>
2016-11-12 18:22:58 +0530

    Initial

--- source/app/services/renderer.js
+++
@@ -1,9 +0,0 @@
-import { ipcRenderer } from 'electron';
-import { WeatherService } from './weather';
-import { NotificationService } from './notification';
-
-console.log("renderer");
-
-ipcRenderer.on('tray-click', (e,args)=> {
-    console.log(args.city);
-});
